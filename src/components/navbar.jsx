2c679e991ec07a3b2a80ca1b4de0a058cc6e40de
baseer4 <sbaseer003@gmail.com>
2025-10-19 18:43:50 +0530

    Fix bug on sm layout

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,6 +5,8 @@ import { Github } from 'lucide-react'
 
 export default function Navbar() {
   return (
+    <div  className="hidden md:block">
+      
    <div className="fixed bottom-4 z-50 inset-x-0 flex justify-center">
       <div className="flex items-center gap-4 backdrop-blur-md bg-white/10 border border-white/15 text-white px-6 py-2 rounded-xl shadow-md transition-all duration-200 hover:shadow-lg hover:bg-white/15">
         {/* Name */}
@@ -35,6 +37,7 @@ export default function Navbar() {
           <Github className="h-4 w-4" />
         </a>
       </div>
+    </div>
     </div>
   )
 }
